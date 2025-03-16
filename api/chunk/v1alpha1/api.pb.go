d990baf417f5b6e67880e3cf7dcf8b9dc939d898
yannic rieger <ybr@76k.io>
2025-03-16 17:39:52 +0100

    goimports -w .

--- api/chunk/v1alpha1/api.pb.go
+++ api/chunk/v1alpha1/api.pb.go
@@ -7,9 +7,10 @@
 package v1alpha1
 
 import (
+	reflect "reflect"
+
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
-	reflect "reflect"
 )
 
 const (
