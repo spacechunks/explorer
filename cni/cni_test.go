b7095c5ce5d4fe88ad78a2aa35ed52fd14ee93e3
yannic rieger <ybr@76k.io>
2025-03-26 22:50:11 +0100

    cni: return an error when an invalid port is received (#67)

--- cni/cni_test.go
+++ cni/cni_test.go
@@ -145,6 +145,74 @@ func TestExecAdd(t *testing.T) {
 			},
 		},
 		{
+			name: "fail when invaild port received",
+			conf: cni.Conf{
+				NetConf: types.NetConf{
+					IPAM: types.IPAM{
+						Type: "host-local",
+					},
+				},
+				PlatformdListenSock: "/some/path",
+			},
+			args: &skel.CmdArgs{
+				ContainerID: "abc",
+				Args:        "K8S_POD_UID=uuidv7",
+				Netns:       "/path/to/netns",
+			},
+			err: cni.ErrInvalidPort,
+			prep: func(
+				h *mock.MockCniHandler,
+				args *skel.CmdArgs,
+				psc *mock.MockV1alpha1ProxyServiceClient,
+				wlc *mock.MockV1alpha2WorkloadServiceClient,
+			) {
+				var (
+					ips = []net.IPNet{
+						{
+							IP:   net.ParseIP("10.10.0.0"),
+							Mask: net.CIDRMask(24, 24),
+						},
+						{
+							IP:   net.ParseIP("10.20.0.0"),
+							Mask: net.CIDRMask(24, 24),
+						},
+					}
+					veth = datapath.VethPair{}
+				)
+
+				h.EXPECT().
+					AllocIPs("host-local", args.StdinData).
+					Return(ips, nil)
+				h.EXPECT().
+					AllocVethPair(args.Netns, ips[0], ips[1]).
+					Return(veth, nil)
+				h.EXPECT().
+					AttachHostVethBPF(veth).
+					Return(nil)
+				h.EXPECT().
+					AttachCtrVethBPF(veth, args.Netns).
+					Return(nil)
+				h.EXPECT().
+					AddDefaultRoute(veth, args.Netns).
+					Return(nil)
+				h.EXPECT().
+					AddFullMatchRoute(veth).
+					Return(nil)
+
+				wlc.EXPECT().
+					WorkloadStatus(mocky.Anything, &workloadv1alpha2.WorkloadStatusRequest{
+						Id: ptr.Pointer("uuidv7"),
+					}).
+					Return(&workloadv1alpha2.WorkloadStatusResponse{
+						Status: &workloadv1alpha2.WorkloadStatus{},
+					}, nil)
+
+				h.EXPECT().
+					DeallocIPs("host-local", args.StdinData).
+					Return(nil)
+			},
+		},
+		{
 			name: "dealloc ips on error",
 			conf: cni.Conf{
 				NetConf: types.NetConf{
@@ -291,7 +359,7 @@ func TestExecAdd(t *testing.T) {
 			tt.prep(h, tt.args, psc, wlc)
 			err := c.ExecAdd(tt.args, tt.conf, psc, wlc)
 			if err != nil && tt.err != nil {
-				assert.EqualError(t, err, tt.err.Error())
+				assert.ErrorAs(t, err, &tt.err)
 				return
 			}
 			assert.NoError(t, err)
