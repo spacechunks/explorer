899e01b3bc014530d096fe73bb096f7142c5dba4
yannic rieger <ybr@76k.io>
2025-05-03 20:04:13 +0200

    api: add flavor versions

--- api/chunk/v1alpha1/api.pb.go
+++ api/chunk/v1alpha1/api.pb.go
@@ -24,11 +24,10 @@
 package v1alpha1
 
 import (
-	reflect "reflect"
-	sync "sync"
-
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
+	reflect "reflect"
+	sync "sync"
 )
 
 const (
