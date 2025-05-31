d1eba51c01ab81fb8ff703dd572a94615668ed99
yannic rieger <ybr@76k.io>
2025-05-31 21:38:19 +0200

    image: add append layer function
    this functions creates a new layer from the passed
    files and appends it to a given base image.

--- controlplane/image/layer.go
+++ controlplane/image/layer.go
@@ -7,28 +7,30 @@ import (
 	"io"
 
 	ociv1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/mutate"
 	"github.com/google/go-containerregistry/pkg/v1/tarball"
+	"github.com/spacechunks/explorer/controlplane/file"
 )
 
-func LayerFromFiles(files map[string][]byte) (ociv1.Layer, error) {
+func LayerFromFiles(files []file.Object) (ociv1.Layer, error) {
 	var (
 		w    bytes.Buffer
 		tarw = tar.NewWriter(&w)
 	)
 
-	for path, data := range files {
+	for _, f := range files {
 		hdr := &tar.Header{
-			Name:     path,
+			Name:     f.Path,
 			Typeflag: tar.TypeReg,
 			Mode:     0777, // FIXME: pass file mode at some point
-			Size:     int64(len(data)),
+			Size:     int64(len(f.Data)),
 		}
 
 		if err := tarw.WriteHeader(hdr); err != nil {
 			return nil, fmt.Errorf("write hdr: %w", err)
 		}
 
-		if _, err := tarw.Write(data); err != nil {
+		if _, err := tarw.Write(f.Data); err != nil {
 			return nil, fmt.Errorf("write file content: %w", err)
 		}
 	}
@@ -40,9 +42,24 @@ func LayerFromFiles(files map[string][]byte) (ociv1.Layer, error) {
 	layer, err := tarball.LayerFromOpener(func() (io.ReadCloser, error) {
 		return io.NopCloser(bytes.NewReader(w.Bytes())), nil
 	})
+
 	if err != nil {
 		return nil, fmt.Errorf("create layer: %w", err)
 	}
 
 	return layer, nil
+}
+
+func AppendLayer(base ociv1.Image, files []file.Object) (ociv1.Image, error) {
+	layer, err := LayerFromFiles(files)
+	if err != nil {
+		return nil, fmt.Errorf("layer from files: %w", err)
+	}
+
+	img, err := mutate.AppendLayers(base, layer)
+	if err != nil {
+		return nil, fmt.Errorf("append layer: %w", err)
+	}
+
+	return img, nil
 }
