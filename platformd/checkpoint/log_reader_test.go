2845416c83dd09b04b482f2a55ff130447cd0b14
yannic rieger <ybr@76k.io>
2025-06-20 23:30:05 +0200

    platformd: add log reader test

---
+++ platformd/checkpoint/log_reader_test.go
@@ -0,0 +1,71 @@
+package checkpoint
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"k8s.io/client-go/tools/remotecommand"
+)
+
+type testRemoteCmdExecutor struct {
+}
+
+func (e *testRemoteCmdExecutor) Stream(_ remotecommand.StreamOptions) error {
+	panic("implement me")
+}
+
+func (e *testRemoteCmdExecutor) StreamWithContext(ctx context.Context, opts remotecommand.StreamOptions) error {
+	t := time.NewTicker(1 * time.Second)
+	counter := 0
+	for {
+		select {
+		case <-t.C:
+			if counter == 3 {
+				_, _ = opts.Stdout.Write([]byte(`Done (30.0s)! For help, type "help"`))
+			}
+			_, _ = opts.Stdout.Write([]byte(fmt.Sprintf("%d", counter)))
+			counter++
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+}
+
+func TestLogReader(t *testing.T) {
+	tests := []struct {
+		name     string
+		deadline time.Duration
+		err      error
+	}{
+		{
+			name:     "works",
+			deadline: 10 * time.Second,
+		},
+		{
+			name:     "timeout reached",
+			deadline: 1 * time.Second,
+			err:      context.DeadlineExceeded,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := newLogReader(&testRemoteCmdExecutor{})
+
+			ctx, cancel := context.WithTimeout(context.Background(), tt.deadline)
+			defer cancel()
+
+			err := r.WaitForRegex(ctx, paperServerReadyRegex)
+
+			if tt.err != nil {
+				require.ErrorIs(t, err, tt.err)
+				return
+			}
+
+			require.NoError(t, err)
+		})
+	}
+}
