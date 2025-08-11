63f92e155193c56b44f8a9e02d8266d6d5a5c5b7
yannic rieger <ybr@76k.io>
2025-08-11 19:50:37 +0200

    the latest flavor version is the first in the list

--- test/fixture/chunk.go
+++ test/fixture/chunk.go
@@ -73,10 +73,6 @@ func Flavor(mod ...func(f *chunk.Flavor)) chunk.Flavor {
 		Versions: []chunk.FlavorVersion{
 			FlavorVersion(nil, func(v *chunk.FlavorVersion) {
 				v.ID = "01953e68-4ca6-73b1-89b4-86455ffd78e7"
-				v.Version = "v1"
-			}),
-			FlavorVersion(nil, func(v *chunk.FlavorVersion) {
-				v.ID = "01953e68-4ca6-73b1-89b4-86455ffd78e7"
 				v.Version = "v2"
 				v.FileHashes = []file.Hash{
 					{
@@ -84,6 +80,10 @@ func Flavor(mod ...func(f *chunk.Flavor)) chunk.Flavor {
 						Hash: "aaaaaaaaaaaaaaaa",
 					},
 				}
+			}),
+			FlavorVersion(nil, func(v *chunk.FlavorVersion) {
+				v.ID = "01953e68-4ca6-73b1-89b4-86455ffd78e7"
+				v.Version = "v1"
 			}),
 		},
 		CreatedAt: time.Date(2025, 2, 23, 13, 12, 15, 0, time.UTC),
