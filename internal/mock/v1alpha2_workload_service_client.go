8902489e79efdbc85f6afa8a899fe7e6ec8b0666
yannic rieger <ybr@76k.io>
2026-03-11 11:24:01 +0100

    platformd: implement stop workload rpc (#204)

--- internal/mock/v1alpha2_workload_service_client.go
+++ internal/mock/v1alpha2_workload_service_client.go
@@ -23,6 +23,80 @@ func (_m *MockV1alpha2WorkloadServiceClient) EXPECT() *MockV1alpha2WorkloadServi
 	return &MockV1alpha2WorkloadServiceClient_Expecter{mock: &_m.Mock}
 }
 
+// StopWorkload provides a mock function with given fields: ctx, in, opts
+func (_m *MockV1alpha2WorkloadServiceClient) StopWorkload(ctx context.Context, in *v1alpha2.WorkloadStopRequest, opts ...grpc.CallOption) (*v1alpha2.WorkloadStopResponse, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, in)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	if len(ret) == 0 {
+		panic("no return value specified for StopWorkload")
+	}
+
+	var r0 *v1alpha2.WorkloadStopResponse
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, *v1alpha2.WorkloadStopRequest, ...grpc.CallOption) (*v1alpha2.WorkloadStopResponse, error)); ok {
+		return rf(ctx, in, opts...)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, *v1alpha2.WorkloadStopRequest, ...grpc.CallOption) *v1alpha2.WorkloadStopResponse); ok {
+		r0 = rf(ctx, in, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*v1alpha2.WorkloadStopResponse)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, *v1alpha2.WorkloadStopRequest, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, in, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// MockV1alpha2WorkloadServiceClient_StopWorkload_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'StopWorkload'
+type MockV1alpha2WorkloadServiceClient_StopWorkload_Call struct {
+	*mock.Call
+}
+
+// StopWorkload is a helper method to define mock.On call
+//   - ctx context.Context
+//   - in *v1alpha2.WorkloadStopRequest
+//   - opts ...grpc.CallOption
+func (_e *MockV1alpha2WorkloadServiceClient_Expecter) StopWorkload(ctx interface{}, in interface{}, opts ...interface{}) *MockV1alpha2WorkloadServiceClient_StopWorkload_Call {
+	return &MockV1alpha2WorkloadServiceClient_StopWorkload_Call{Call: _e.mock.On("StopWorkload",
+		append([]interface{}{ctx, in}, opts...)...)}
+}
+
+func (_c *MockV1alpha2WorkloadServiceClient_StopWorkload_Call) Run(run func(ctx context.Context, in *v1alpha2.WorkloadStopRequest, opts ...grpc.CallOption)) *MockV1alpha2WorkloadServiceClient_StopWorkload_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		variadicArgs := make([]grpc.CallOption, len(args)-2)
+		for i, a := range args[2:] {
+			if a != nil {
+				variadicArgs[i] = a.(grpc.CallOption)
+			}
+		}
+		run(args[0].(context.Context), args[1].(*v1alpha2.WorkloadStopRequest), variadicArgs...)
+	})
+	return _c
+}
+
+func (_c *MockV1alpha2WorkloadServiceClient_StopWorkload_Call) Return(_a0 *v1alpha2.WorkloadStopResponse, _a1 error) *MockV1alpha2WorkloadServiceClient_StopWorkload_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *MockV1alpha2WorkloadServiceClient_StopWorkload_Call) RunAndReturn(run func(context.Context, *v1alpha2.WorkloadStopRequest, ...grpc.CallOption) (*v1alpha2.WorkloadStopResponse, error)) *MockV1alpha2WorkloadServiceClient_StopWorkload_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // WorkloadStatus provides a mock function with given fields: ctx, in, opts
 func (_m *MockV1alpha2WorkloadServiceClient) WorkloadStatus(ctx context.Context, in *v1alpha2.WorkloadStatusRequest, opts ...grpc.CallOption) (*v1alpha2.WorkloadStatusResponse, error) {
 	_va := make([]interface{}, len(opts))
