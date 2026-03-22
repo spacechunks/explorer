1a6d63d64ff357b7d61373b9be349a6a188888ac
yannic rieger <10352239+freggy@users.noreply.github.com>
2026-03-22 17:43:42 +0100

    controlplane: sanatize server configs (#205)
    we want to avoid that users upload configs that will lead to broken
    servers (e.g. player not being able to join, because wrong server port
    etc.). so we change them before building the checkpoint.

---
+++ controlplane/serverconfig/paper.go
@@ -0,0 +1,52 @@
+package serverconfig
+
+import (
+	"fmt"
+
+	"github.com/goccy/go-yaml"
+	"github.com/spacechunks/explorer/controlplane/blob"
+)
+
+/*
+proxies:
+	bungee-cord:
+	  online-mode: true
+	proxy-protocol: false
+	velocity:
+	  enabled: false
+	  online-mode: true
+	  secret: ""
+*/
+
+type paperGlobal struct {
+	Proxies proxiesConfig `json:"proxies"`
+}
+
+type proxiesConfig struct {
+	ProxyProtocol bool `json:"proxy-protocol"`
+	Velocity      struct {
+		Enabled    bool   `json:"enabled"`
+		OnlineMode bool   `json:"online-mode"`
+		Secret     string `json:"secret"`
+	}
+}
+
+func sanatizePaperGlobal(data []byte) ([]byte, error) {
+	var global paperGlobal
+	if err := yaml.Unmarshal(data, &blob.Object{}); err != nil {
+		return nil, fmt.Errorf("unmarshal: %w", err)
+	}
+
+	global.Proxies.ProxyProtocol = false
+
+	global.Proxies.Velocity.Enabled = true
+	global.Proxies.Velocity.OnlineMode = true
+	global.Proxies.Velocity.Secret = ""
+
+	ret, err := yaml.Marshal(global)
+	if err != nil {
+		return nil, fmt.Errorf("marshal: %w", err)
+	}
+
+	return ret, nil
+}
