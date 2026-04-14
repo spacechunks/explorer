f7f4f35e26e4a4de232c2f6b350418ccd05559f1
yannic rieger <ybr@76k.io>
2026-04-14 20:31:52 +0200

    insert more globally needed data

--- test/fixture/control_plane.go
+++ test/fixture/control_plane.go
@@ -106,6 +106,9 @@ func (c ControlPlane) Run(t *testing.T, opts ...ControlPlaneRunOption) {
 	}
 
 	// seed data that is globally needed
+	c.Postgres.InsertMinecraftVersion(t)
+	c.Postgres.InsertNode(t)
+
 	_, err := c.Postgres.Pool.Exec(
 		ctx,
 		`INSERT INTO minecraft_versions (version, image_url) VALUES ($1, $2)`,
