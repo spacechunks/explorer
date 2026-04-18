c8bbd5882929d1994b519c688edf5eb970c0011a
yannic rieger <ybr@76k.io>
2026-04-18 19:35:34 +0200

    implement archive worker

---
+++ controlplane/worker/archive_test.go
@@ -0,0 +1,217 @@
+package worker_test
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"testing"
+
+	"github.com/spacechunks/explorer/controlplane/resource"
+	"github.com/spacechunks/explorer/controlplane/worker"
+	"github.com/spacechunks/explorer/internal/mock"
+	"github.com/spacechunks/explorer/test/fixture"
+	mocky "github.com/stretchr/testify/mock"
+)
+
+func TestArchiveFullChunk(t *testing.T) {
+	var (
+		c = fixture.Chunk(func(tmp *resource.Chunk) {
+			tmp.Flavors[0].Versions[0].BuildStatus = resource.FlavorVersionBuildStatusCompleted
+			tmp.Flavors[0].Versions[1].BuildStatus = resource.FlavorVersionBuildStatusCompleted
+
+			tmp.Flavors[1].Versions[0].BuildStatus = resource.FlavorVersionBuildStatusCompleted
+			tmp.Flavors[1].Versions[1].BuildStatus = resource.FlavorVersionBuildStatusCompleted
+		})
+		logger          = slog.New(slog.NewTextHandler(os.Stdout, nil))
+		mockArchiveRepo = mock.NewMockChunkArchiveRepository(t)
+		mockChunkRepo   = mock.NewMockChunkRepository(t)
+		mockInsRepo     = mock.NewMockInstanceRepository(t)
+	)
+
+	mockChunkRepo.
+		EXPECT().
+		AllDeletedFlavors(mocky.Anything).
+		Return(map[string]string{
+			c.Flavors[0].ID: c.ID,
+			c.Flavors[1].ID: c.ID,
+		}, nil)
+
+	for _, f := range c.Flavors {
+		tmp := f
+		tmp.Versions = nil
+
+		mockChunkRepo.
+			EXPECT().
+			GetFlavorByID(mocky.Anything, f.ID).
+			Return(f, nil).
+			Once()
+
+		mockChunkRepo.
+			EXPECT().
+			GetFlavorByID(mocky.Anything, f.ID).
+			Return(tmp, nil).
+			Once()
+
+		for _, v := range f.Versions {
+			mockInsRepo.
+				EXPECT().
+				CountInstancesByFlavorVersionID(mocky.Anything, v.ID).
+				Return(uint(0), nil)
+
+			mockArchiveRepo.
+				EXPECT().
+				ArchiveFlavorVersion(mocky.Anything, f.ID, v).
+				Return(nil)
+		}
+
+		mockArchiveRepo.
+			EXPECT().
+			ArchiveFlavor(
+				mocky.Anything,
+				c.ID,
+				mocky.MatchedBy(func(flavor resource.Flavor) bool {
+					return flavor.ID == f.ID
+				}),
+			).
+			Return(nil)
+	}
+
+	withoutFlavors := fixture.Chunk(func(tmp *resource.Chunk) {
+		tmp.Flavors = nil
+	})
+
+	mockChunkRepo.
+		EXPECT().
+		GetChunkByID(mocky.Anything, c.ID).
+		Return(withoutFlavors, nil)
+
+	mockArchiveRepo.
+		EXPECT().
+		ArchiveChunk(mocky.Anything, withoutFlavors).
+		Return(nil)
+
+	w := worker.NewArchiveWorker(logger, mockChunkRepo, mockInsRepo, mockArchiveRepo)
+
+	_ = w.Work(context.Background(), nil)
+}
+
+func TestDoNotArchiveFlavorVersionsCurrentlyBuilding(t *testing.T) {
+	tests := []struct {
+		name        string
+		buildStatus resource.FlavorVersionBuildStatus
+	}{
+		{
+			name:        "build image",
+			buildStatus: resource.FlavorVersionBuildStatusBuildImage,
+		},
+		{
+			name:        "build checkpoint",
+			buildStatus: resource.FlavorVersionBuildStatusBuildCheckpoint,
+		},
+		{
+			name:        "build pending",
+			buildStatus: resource.FlavorVersionBuildStatusPending,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var (
+				c = fixture.Chunk(func(tmp *resource.Chunk) {
+					tmp.Flavors = []resource.Flavor{
+						fixture.Flavor(func(tmpF *resource.Flavor) {
+							tmpF.Versions = []resource.FlavorVersion{
+								fixture.FlavorVersion(func(tmpV *resource.FlavorVersion) {
+									tmpV.BuildStatus = tt.buildStatus
+								}),
+							}
+						}),
+					}
+				})
+				logger          = slog.New(slog.NewTextHandler(os.Stdout, nil))
+				mockArchiveRepo = mock.NewMockChunkArchiveRepository(t)
+				mockChunkRepo   = mock.NewMockChunkRepository(t)
+				mockInsRepo     = mock.NewMockInstanceRepository(t)
+			)
+
+			f := c.Flavors[0]
+
+			mockChunkRepo.
+				EXPECT().
+				AllDeletedFlavors(mocky.Anything).
+				Return(map[string]string{
+					c.Flavors[0].ID: c.ID,
+				}, nil)
+
+			mockChunkRepo.
+				EXPECT().
+				GetFlavorByID(mocky.Anything, f.ID).
+				Return(f, nil)
+
+			mockInsRepo.
+				EXPECT().
+				CountInstancesByFlavorVersionID(mocky.Anything, f.Versions[0].ID).
+				Return(uint(0), nil)
+
+			mockArchiveRepo.AssertNotCalled(t, "ArchiveFlavorVersion", f.ID, f.Versions[0])
+
+			mockChunkRepo.
+				EXPECT().
+				GetChunkByID(mocky.Anything, c.ID).
+				Return(c, nil)
+
+			w := worker.NewArchiveWorker(logger, mockChunkRepo, mockInsRepo, mockArchiveRepo)
+
+			_ = w.Work(context.Background(), nil)
+		})
+	}
+}
+
+func TestArchiveWorkerDoesNotArchiveFlavorAndChunkWhenVersionsAndFlavorsRemain(t *testing.T) {
+	var (
+		c = fixture.Chunk()
+		f = c.Flavors[0]
+
+		logger          = slog.New(slog.NewTextHandler(os.Stdout, nil))
+		mockArchiveRepo = mock.NewMockChunkArchiveRepository(t)
+		mockChunkRepo   = mock.NewMockChunkRepository(t)
+		mockInsRepo     = mock.NewMockInstanceRepository(t)
+	)
+
+	mockChunkRepo.
+		EXPECT().
+		AllDeletedFlavors(mocky.Anything).
+		Return(map[string]string{
+			f.ID: c.ID,
+		}, nil)
+
+	mockChunkRepo.
+		EXPECT().
+		GetFlavorByID(mocky.Anything, f.ID).
+		Return(f, nil).
+		Once()
+
+	mockChunkRepo.
+		EXPECT().
+		GetFlavorByID(mocky.Anything, f.ID).
+		Return(f, nil).
+		Once()
+
+	for _, v := range f.Versions {
+		mockInsRepo.
+			EXPECT().
+			CountInstancesByFlavorVersionID(mocky.Anything, v.ID).
+			Return(uint(0), nil)
+	}
+
+	mockChunkRepo.
+		EXPECT().
+		GetChunkByID(mocky.Anything, c.ID).
+		Return(c, nil)
+
+	w := worker.NewArchiveWorker(logger, mockChunkRepo, mockInsRepo, mockArchiveRepo)
+
+	_ = w.Work(context.Background(), nil)
+
+	mockArchiveRepo.AssertNotCalled(t, "ArchiveFlavor", mocky.Anything, mocky.Anything, mocky.Anything)
+	mockArchiveRepo.AssertNotCalled(t, "ArchiveChunk", mocky.Anything, mocky.Anything, mocky.Anything)
+}
--- internal/mock/chunk_repository.go
+++ internal/mock/chunk_repository.go
@@ -81,6 +81,64 @@ func (_c *MockChunkRepository_AllChunkThumbnailHashes_Call) RunAndReturn(run fun
 	return _c
 }
 
+// AllDeletedFlavors provides a mock function with given fields: ctx
+func (_m *MockChunkRepository) AllDeletedFlavors(ctx context.Context) (map[string]string, error) {
+	ret := _m.Called(ctx)
+
+	if len(ret) == 0 {
+		panic("no return value specified for AllDeletedFlavors")
+	}
+
+	var r0 map[string]string
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context) (map[string]string, error)); ok {
+		return rf(ctx)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context) map[string]string); ok {
+		r0 = rf(ctx)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(map[string]string)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context) error); ok {
+		r1 = rf(ctx)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// MockChunkRepository_AllDeletedFlavors_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'AllDeletedFlavors'
+type MockChunkRepository_AllDeletedFlavors_Call struct {
+	*mock.Call
+}
+
+// AllDeletedFlavors is a helper method to define mock.On call
+//   - ctx context.Context
+func (_e *MockChunkRepository_Expecter) AllDeletedFlavors(ctx interface{}) *MockChunkRepository_AllDeletedFlavors_Call {
+	return &MockChunkRepository_AllDeletedFlavors_Call{Call: _e.mock.On("AllDeletedFlavors", ctx)}
+}
+
+func (_c *MockChunkRepository_AllDeletedFlavors_Call) Run(run func(ctx context.Context)) *MockChunkRepository_AllDeletedFlavors_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(context.Context))
+	})
+	return _c
+}
+
+func (_c *MockChunkRepository_AllDeletedFlavors_Call) Return(_a0 map[string]string, _a1 error) *MockChunkRepository_AllDeletedFlavors_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *MockChunkRepository_AllDeletedFlavors_Call) RunAndReturn(run func(context.Context) (map[string]string, error)) *MockChunkRepository_AllDeletedFlavors_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // CreateChunk provides a mock function with given fields: ctx, _a1
 func (_m *MockChunkRepository) CreateChunk(ctx context.Context, _a1 resource.Chunk) (resource.Chunk, error) {
 	ret := _m.Called(ctx, _a1)
