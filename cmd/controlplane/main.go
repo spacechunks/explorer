1fbba596baa4f8868a417b48c18e1575b6725466
yannic rieger <10352239+freggy@users.noreply.github.com>
2026-04-19 18:51:02 +0200

    controlplane: get chunk by id also includes deleted flavors (#228)

--- cmd/controlplane/main.go
+++ cmd/controlplane/main.go
@@ -69,7 +69,7 @@ func main() {
 		packTextureDir           = fs.String("resource-pack-texture-dir", "", "path inside the resource pack to the directory where the textures will live. e.g. assets/mynamespace/textures/item") //nolint:lll
 		velocitySecret           = fs.String("velocity-secret", "", "the velocity secret to set in the paper server configuration")                                                                 //nolint:lll
 		changeSetTarballMaxSize  = fs.Uint64("change-set-tarball-max-size", 1073741824, "the maximum allowed size in bytes of the change set tarball")                                              //nolint:lll
-		archiveInterval          = fs.Duration("archiveInterval", 3*time.Minute, "in what interval the deleted chunks and flavors should be archived")                                              //nolint:lll
+		archiveInterval          = fs.Duration("archive-interval", 3*time.Minute, "in what interval the deleted chunks and flavors should be archived")                                             //nolint:lll
 	)
 	if err := ff.Parse(fs, os.Args[1:],
 		ff.WithEnvVarPrefix("CONTROLPLANE"),
