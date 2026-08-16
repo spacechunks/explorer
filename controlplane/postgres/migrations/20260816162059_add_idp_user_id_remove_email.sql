9aaedaca5d32e2a3eb685569d47d1980067f89b3
yannic rieger <10352239+freggy@users.noreply.github.com>
2026-08-16 20:11:41 +0200

    controlplane: fix migration

--- controlplane/postgres/migrations/20260816162059_add_idp_user_id_remove_email.sql
+++ controlplane/postgres/migrations/20260816162059_add_idp_user_id_remove_email.sql
@@ -1,5 +1,5 @@
 -- migrate:up
-ALTER TABLE users ADD COLUMN idp_id VARCHAR(256) UNIQUE NOT NULL DEFAULT '';
+ALTER TABLE users ADD COLUMN idp_id VARCHAR(256) NOT NULL DEFAULT '';
 ALTER TABLE users DROP COLUMN email;
 
 -- migrate:down
