a6a4e3edd83ac586cc2077d3d4fe4cc0248792bf
Konrad Trubiło <konradtrubilo.kt@gmail.com>
2026-09-22 18:56:50 +0200

    fix: lint problem

--- controlplane/chunk/flavor_test.go
+++ controlplane/chunk/flavor_test.go
@@ -752,7 +752,6 @@ func TestBuildFlavorVersion(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-
 			var (
 				ctx        = context.Background()
 				versionID  = test.NewUUIDv7(t)
